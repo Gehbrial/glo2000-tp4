fbc699284ac2134221a1f8ce332e5464c9cfd58c
Dalloriam <dalloriam@gmail.com>
2017-11-28 17:31:30 -0500

    Add pw regex

--- user.py
+++ user.py
@@ -1,11 +1,16 @@
 from hashlib import sha256
 
 import os
+import re
+
+PASSWORD_PATTERN = re.compile("^(?=.*[A-Za-z])(?=.*\d).{6,12}$")
 
 
-def init():
-    if not os.path.isdir('users'):
-        os.mkdir('users')
+def _enforce_password_rules(password):
+    if not PASSWORD_PATTERN.match(password):
+        raise ValueError(
+            "Le mot de passe doit être en 6 et 12 caracteres et doit comporter une lettre ainsi qu'un chiffre"
+        )
 
 
 def _hash_user_password(password):
@@ -14,37 +19,32 @@ def _hash_user_password(password):
 
 
 def _read_user_password(username):
-    config_pth = os.path.join(_get_user_folder_path(username), 'config.txt')
+    config_pth = os.path.join(username, 'config.txt')
     with open(config_pth, 'rU') as infile:
         return infile.read()
 
 
-def _get_user_folder_path(username):
-    return os.path.join('users', username)
-
-
 def _user_exists(username):
-    return os.path.isdir(_get_user_folder_path(username))
+    return os.path.isdir(username)
 
 
 def create_user(username, password):
-    init()
 
     if _user_exists(username):
         raise ValueError("L'utilisateur {} existe deja".format(username))
 
-    # Creation du dossier de l'utilisateur
-    user_path = _get_user_folder_path(username)
-    os.mkdir(user_path)
+    _enforce_password_rules(password)
 
-    config_path = os.path.join(user_path, 'config.txt')
+    # Creation du dossier de l'utilisateur
+    os.mkdir(username)
+
+    config_path = os.path.join(username, 'config.txt')
 
     with open(config_path, 'w') as config_file:
         config_file.write(_hash_user_password(password))
 
 
 def validate_password(username, password):
-    init()
     if not _user_exists(username):
         raise ValueError("L'utilisateur {} n'existe pas".format(username))
 
